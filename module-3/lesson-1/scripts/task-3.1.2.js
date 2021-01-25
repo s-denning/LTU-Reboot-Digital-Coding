c3657b995d419c72ec8e5f221c59342a9e572646
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-25 10:29:57 +0000

    Update task-3.1.2.js
    add alert, console message and document.write example

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,7 @@
+alert('This is an alert message'); //pop up message box on opening page
+
+/*below is a message that will appear in the console 
+*/
+console.log('this is a message from the console');
+
+document.write('hello from the document');
