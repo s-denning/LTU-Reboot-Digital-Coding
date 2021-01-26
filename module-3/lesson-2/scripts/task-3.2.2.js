06bccf31b908e65de5a0623569a65afe2d9a214d
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-26 10:14:38 +0000

    Update task-3.2.2.js
    add function to combine first + last name, and print full name

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,13 @@
+//  1. Write a simple program to combine a first name and a last name inside a function.
+
+//Then update the function to accept a first and last name as arguments.
+
+var firstName = 'Stan '
+var lastName = 'Denning'
+
+function fullName() {
+    let fullName = firstName + lastName 
+    document.write(fullName)
+}
+
+fullName()
