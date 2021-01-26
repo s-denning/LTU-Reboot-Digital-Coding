e8f038230323f26ab6e756fae28433af3b256544
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-26 10:01:08 +0000

    Update task-3.2.1.js
    create and called function

---
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -0,0 +1,6 @@
+function codingLesson() {
+    document.write('I am writing a javaScript function for module 3.');
+    document.write(" The lesson I'm writing this function for is lesson 2 from module 3.");
+}
+
+codingLesson();
