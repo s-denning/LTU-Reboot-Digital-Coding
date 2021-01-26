de4ae6cc283c6e9c023f210fe8a4c7d14c85c93b
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-26 11:08:05 +0000

    Update task-3.2.4.js
    created function to advise on putting on coat depending on temperature variable

---
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -0,0 +1,14 @@
+let temperature = 40
+
+function coatWeather() {
+if (temperature <50){ 
+    document.write('put your coat on!');
+} else {document.write('no worries')};
+};
+
+
+coatWeather();
+
+//  1. Make a variable called "temperature"
+
+// 2. Write some code that tells you to put on a coat if it is below 50 degrees.
