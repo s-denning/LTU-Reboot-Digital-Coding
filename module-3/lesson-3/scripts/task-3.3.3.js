d2d5b55207e6be5316a828f40607db5fd26212c3
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-27 11:13:53 +0000

    Update task-3.3.3.js
    add basic calculator function, some syntax wrong after first PLUS function, needs checking against others.

---
+++ module-3/lesson-3/scripts/task-3.3.3.js
@@ -0,0 +1,36 @@
+
+
+function calculator(number1, number2, operator) {
+
+    result = '';
+
+
+    switch(operator) {
+case '+' :
+    result = number1 + number2
+    console.log(number1 + ' + ' + number2 + '=' +result);
+    break;
+
+    //above works fine, others have syntax issue. work out problem by comparing
+case '*' :
+    result = number1 * number2
+    console.log(number1 * + ' * ' +  number2 + '=' +result);
+break;
+case '-' :
+    result = number1 - number2
+    console.log(number1 - '-' +  number2 + '=' +result);
+break;
+case '/' :
+    result = number1 / number2
+    console.log(number1 / '/' +  number2 + '=' +result);
+break;
+case 'square' :
+    result = number1 * number1
+    console.log(number1 * '*' +  number1 + '=' +result);
+    break;
+    }
+
+
+}
+
+calculator(10, 5, '*')
