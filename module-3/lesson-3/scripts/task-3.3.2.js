d3abd60fec9a9e9666798747434a1ef5e5febefb
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-27 10:44:06 +0000

    Update task-3.3.2.js
    add note

--- module-3/lesson-3/scripts/task-3.3.2.js
+++ module-3/lesson-3/scripts/task-3.3.2.js
@@ -1,16 +1,7 @@
-//1. Create a function named drinkOrder
 
-//2. Add 2 arguments for “size” and “drink”
 
-//3. Use a switch statement to determine where the functional code needs to be written
-
-//4. Create a message in each case statement to be returned.
-
-//5. Console.log the returned message
-
-//6. Commit your changes
-
-//. Push your changes to remote
+// switch statements - switch(variable) 
+//{ case 'variable example', action (console log), break, repeat}
 
 
 function drinkOrder(size, drink) {
