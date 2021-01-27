4cd9fcb4f204cdabdfee3142241c38d06b532af2
s-denning <77330263+s-denning@users.noreply.github.com>
2021-01-27 09:51:51 +0000

    Update task-3.2.4.js
    add logical operator for checking weather against saeson

--- module-3/lesson-2/scripts/task-3.2.4.js
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -1,15 +1,18 @@
-let temperature = 15
+
+
+let temperature = 4;
+var season = 'spring';
 //put functions in temperature going from least to most otherwise it won't work properly
-function coatWeather() {
-if (temperature <1){ 
+
+if (temperature <5 && season === 'spring'){ 
     document.write('stay inside!');
 } else if (temperature <20){
     document.write('put on a coat and a hat!')
 } else if (temperature <50){
     document.write('put your coat on!')
 } else {document.write('no worries')};
-};
 
 
 coatWeather();
 
+//add logical operater a && b 
