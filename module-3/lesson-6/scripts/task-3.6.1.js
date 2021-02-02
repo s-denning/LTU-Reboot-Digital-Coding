c28aea03e141d2b54dadfe3f5c8ed93cc5e1f02c
s-denning <77330263+s-denning@users.noreply.github.com>
2021-02-02 10:54:06 +0000

    Create task-3.6.1.js
    changed header image and second paragraph nodes

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,6 @@
+var logo = document.querySelector('img');
+    logo.setAttribute('src', 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png');
+
+var secondParagraph = document.querySelectorAll('p')[1];
+    secondParagraph.style.border = '4px solid goldenrod';
+    secondParagraph.style.marginTop = '60px'
